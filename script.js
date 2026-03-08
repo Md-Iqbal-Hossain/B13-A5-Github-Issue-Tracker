737b6cff4c4b1a342bf703afc9537c3589c17276
Iqbal <iqbalhossain6615@gmail.com>
2026-03-09 02:04:54 +0600

    modal added into cards

--- script.js
+++ script.js
@@ -32,17 +32,17 @@ function switchTab(tab) {
 
     setTimeout(() => {
 
-    if (tab === 'all') {
-        displayIssues(allIssues);
-    }
-    else {
-        const filtered = allIssues.filter(issue => issue.status === tab);
-        displayIssues(filtered);
-    }
+        if (tab === 'all') {
+            displayIssues(allIssues);
+        }
+        else {
+            const filtered = allIssues.filter(issue => issue.status === tab);
+            displayIssues(filtered);
+        }
 
-    hideLoading();
+        hideLoading();
 
-}, 300);
+    }, 300);
 }
 
 function showLoading() {
@@ -75,7 +75,8 @@ function displayIssues(issues) {
     issues.forEach(issue => {
         console.log(issue);
         const card = document.createElement('div');
-        card.className = `card bg-base-100 w-full shadow-sm border-t-4 ${issue.status === 'open' ? 'border-green-500' : 'border-purple-500'}`;
+        card.onclick = () => loadIssueDetails(issue.id);
+        card.className = `cursor-pointer card bg-base-100 w-full shadow-sm border-t-4 ${issue.status === 'open' ? 'border-green-500' : 'border-purple-500'}`;
 
         // status image
         const statusImage = issue.status === 'open'
@@ -145,6 +146,100 @@ function displayIssues(issues) {
 
 
     })
+}
+
+async function loadIssueDetails(id) {
+    const res = await fetch(`https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`);
+    const data = await res.json();
+    const issue = data.data;
+    showIssueModal(issue);
+}
+
+function showIssueModal(issue) {
+
+    const dateObj = new Date(issue.createdAt);
+    const formattedDate = `${dateObj.getDate()}/${dateObj.getMonth() + 1}/${dateObj.getFullYear()}`;
+
+    const labelColors = {
+        'bug': 'bg-red-100 text-red-600 border-1 border-red-300',
+        'help wanted': 'bg-yellow-100 text-yellow-600 border-1 border-yellow-300',
+        'documentation': 'bg-gray-100 text-gray-600 border-1 border-gray-300',
+        'good first issue': 'bg-blue-100 text-blue-600 border-1 border-blue-300',
+        'enhancement': 'bg-green-100 text-green-600 border-1 border-green-300'
+    };
+
+    function formatName(str) {
+        if (!str) return "";
+        return str
+            .replaceAll("_", " ")
+            .split(" ")
+            .map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+            .join(" ");
+    }
+
+    document.getElementById("modalTitle").innerText = issue.title;
+    document.getElementById("modalDescription").innerText = issue.description;
+    const modalAuthor = document.getElementById("modalAuthor");
+
+    let authorName = "Not Found";
+    if (issue.author && issue.author.trim() !== "") {
+        authorName = formatName(issue.author);
+    }
+
+    modalAuthor.innerText = `Opened by ${authorName}`;
+
+    document.getElementById("modalDate").innerText = formattedDate;
+
+    const statusBadge = document.getElementById("modalStatus");
+
+    statusBadge.innerText = issue.status.toUpperCase();
+
+    if (issue.status === "open") {
+        statusBadge.className = "badge bg-green-500 text-white rounded-xl";
+    }
+    else {
+        statusBadge.className = "badge bg-purple-500 text-white rounded-xl";
+    }
+
+    if (!issue.assignee || issue.assignee.trim() === "") {
+        modalAssignee.innerText = "Not Found";
+    } else {
+        modalAssignee.innerText = formatName(issue.assignee);
+    }
+
+    const modalPriority = document.getElementById("modalPriority");
+
+    let priorityClass = '';
+    if (issue.priority.toLowerCase() === 'high') {
+        priorityClass = 'bg-red-500';
+    } else if (issue.priority.toLowerCase() === 'medium') {
+        priorityClass = 'bg-yellow-500';
+    } else {
+        priorityClass = 'bg-gray-500';
+    }
+
+    modalPriority.className = `badge ${priorityClass} text-white rounded-xl`;
+    modalPriority.innerText = issue.priority.toUpperCase();
+
+    const labelsContainer = document.getElementById("modalLabels");
+
+    labelsContainer.innerHTML = "";
+
+    issue.labels.forEach(label => {
+        const lowerLabel = label.toLowerCase();
+        const colorClass = labelColors[lowerLabel] || 'bg-gray-100 text-gray-600';
+
+        let icon = '';
+        if (lowerLabel === 'bug') icon = '<img src="assets/BugDroid.png" class="w-3 h-3" alt=""> ';
+        else if (lowerLabel === 'help wanted') icon = '<img src="assets/Lifebuoy.png" class="w-3 h-3" alt=""> ';
+
+        const badge = document.createElement("div");
+        badge.className = `badge badge-soft rounded-2xl ${colorClass} flex items-center gap-1 text-xs md:text-[12px] lg:text-[14px] px-2 py-1`;
+        badge.innerHTML = `${icon}${label.replaceAll("_", " ")}`;
+        labelsContainer.appendChild(badge);
+    });
+
+    document.getElementById("issue_detail_modal").showModal();
 }
 
 switchTab(currentTab);
